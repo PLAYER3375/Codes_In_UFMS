c87b6917218b392d34fe1a1d583cbb8973269f05
PLAYER3375 <matheus_gomes@ufms.br>
2025-10-20 13:55:34 -0300

    mexi mais um pouco

--- Estudando_ALG2/TrabalhoAlg2/pilha.c
+++ Estudando_ALG2/TrabalhoAlg2/pilha.c
@@ -1,19 +1,27 @@
 #include"pilha.h"
 
-void colocaInicio(stock **p, char nome[max]){
+void colocaInicio(stock **p, char nome[max], int espessura){
     stock *aux;
 
     aux=alocaNovo();
     strcpy(aux->corChapa, nome);
+    aux->espessuraChapa=espessura;
     aux->prox=*p;
     *p=aux;
 }
 
 void atualizaEstoque(stock **p){
     FILE *arqv;
+    char nome[max];
+    int espessura;
     arqv=fopen("estoque.txt", "r");
     if(arqv==NULL)
         return;
 
-    
+    while(!feof){
+        fscanf("%[^,], %d;", nome, espessura);
+        colocaInicio(p, nome, espessura);
+    }
+
+    printf("Estoque atualizado com sucesso!!!\n");
 }
