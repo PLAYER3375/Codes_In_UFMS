37fc9c9cbde822440173f72066bd9ee4fa1db7f1
PLAYER3375 <matheus_gomes@ufms.br>
2025-12-18 11:26:21 -0300

    comecei a ver java

---
+++ Estudando_POO/FirstProject/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x;
+        int y;
+
+        x=1;
+        y=32;
+        
+        System.out.printf("joão pulou %d %s", x+y, "vezes");
+
+    }
+}
